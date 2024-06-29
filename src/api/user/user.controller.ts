c71760f403aa1cbd32005defdd6e3d51b7228faa
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-06-29 17:00:51 +0530

    add controller

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,5 +1,20 @@
+import { Body, Get, JsonController, Post } from "routing-controllers";
+import { getUsersDTO, SignUpDto } from "./user.dto";
+
+@JsonController('/users')
 class UserController {
 
+
+  @Get('/')
+  async getUsers(){
+    return 'returning all users'
+  }
+
+  @Post('/')
+  async addUser(@Body() body: SignUpDto): Promise<getUsersDTO>{
+    const user = new getUsersDTO(body.name, body.userName, 'sadfsdf');
+    return user;
+  }
 }
 
 export default UserController
