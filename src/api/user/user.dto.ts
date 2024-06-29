31c70f3f37cab56949240548eef1a275f686df40
Siddharth Bisht <siddharthbisht31@gmail.com>
2024-06-29 16:57:06 +0530

    add dto

---
+++ src/api/user/user.dto.ts
@@ -0,0 +1,35 @@
+import { IsString, MaxLength, MinLength } from 'class-validator'
+
+export class SignUpDto{
+
+  @IsString()
+  @MaxLength(30)
+  @MinLength(10)
+  name: string;
+
+  @IsString()
+  @MaxLength(30)
+  @MinLength(10)
+  userName: string;
+
+  constructor(name: string, userName: string){
+    this.name = name;
+    this.userName = userName;
+  }
+}
+
+
+export class getUsersDTO{
+  name: string;
+  userName: string;
+  id: string// or int if using rdbms
+
+  constructor(name: string, userName: string, id: string){
+    this.name = name;
+    this.userName = userName;
+    this.id = id
+  }
+}
+
+
+
